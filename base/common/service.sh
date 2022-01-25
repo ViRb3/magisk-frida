8471e0796723fc6d9692f171db9447c8ae0fda14
Victor <virb3e@gmail.com>
2022-01-25 21:36:47 +0000

    Sleep extra time to ensure boot is completed

--- base/common/service.sh
+++ base/common/service.sh
@@ -7,10 +7,16 @@
 MODDIR=${0%/*}
 
 # This script will be executed in late_start service mode
+
+# wait for boot to complete
 while [ "$(getprop sys.boot_completed)" != 1 ]; do
     sleep 1
 done
 
+# ensure boot has actually completed
+sleep 5
+
+# restart on crash
 while true; do
     frida-server -D
     sleep 1
